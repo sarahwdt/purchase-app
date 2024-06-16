0e1dd79d6870d881864b8fc614eda1605cd8656e
Sarah <sarahwilldietoday@gmail.com>
2024-06-16 17:58:34 +0400

    Init spring app

---
+++ src/main/java/org/leroyjenkins/paymenttestapp/PaymentTestAppApplication.java
@@ -0,0 +1,13 @@
+package org.leroyjenkins.paymenttestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTestAppApplication.class, args);
+    }
+
+}
