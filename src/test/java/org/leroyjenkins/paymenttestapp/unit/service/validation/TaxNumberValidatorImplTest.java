8c2a7176056420435f78c11c8e537defd6d33233
Sarah <sarahwilldietoday@gmail.com>
2024-07-08 20:10:46 +0400

    Add TaxNumberValidatorImplTest

---
+++ src/test/java/org/leroyjenkins/paymenttestapp/unit/service/validation/TaxNumberValidatorImplTest.java
@@ -0,0 +1,39 @@
+package org.leroyjenkins.paymenttestapp.unit.service.validation;
+
+import org.junit.jupiter.api.Test;
+import org.leroyjenkins.paymenttestapp.exception.TaxCalculationException;
+import org.leroyjenkins.paymenttestapp.service.TaxService;
+import org.leroyjenkins.paymenttestapp.service.validation.impl.TaxNumberValidatorImpl;
+import org.leroyjenkins.paymenttestapp.unit.service.AbstractUnitTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.when;
+
+class TaxNumberValidatorImplTest extends AbstractUnitTest {
+    @Mock
+    private TaxService taxService;
+
+    @InjectMocks
+    private TaxNumberValidatorImpl validator;
+
+    @Test
+    void Should_ThrowException_When_validateTaxNumberIsCalledAndTaxCannotBeCalculated() {
+        String taxNumber = "tax-number";
+        TaxCalculationException expectedException = new TaxCalculationException(taxNumber);
+        when(taxService.isTaxCanBeCalculated(taxNumber)).thenReturn(false);
+
+        assertThatThrownByIsTheSameAs(() -> validator.validateTaxNumber(taxNumber),
+                expectedException);
+    }
+
+    @Test
+    void Should_DoNothing_When_validateTaxNumberIsNotCalledAndTaxCanBeCalculated() {
+        String taxNumber = "tax-number";
+        when(taxService.isTaxCanBeCalculated(taxNumber)).thenReturn(true);
+
+        assertThatCode(() -> validator.validateTaxNumber(taxNumber))
+                .doesNotThrowAnyException();
+    }
+}
