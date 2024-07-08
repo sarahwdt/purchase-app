9024e7b3f0d18fafba8d0da46040a1596ecf1da5
Sarah <sarahwilldietoday@gmail.com>
2024-07-08 20:16:52 +0400

    Add PaymentProcessorValidatorImplTest

---
+++ src/test/java/org/leroyjenkins/paymenttestapp/unit/service/validation/PaymentProcessorValidatorImplTest.java
@@ -0,0 +1,38 @@
+package org.leroyjenkins.paymenttestapp.unit.service.validation;
+
+import org.junit.jupiter.api.Test;
+import org.leroyjenkins.paymenttestapp.exception.PaymentProcessorNotRegisteredException;
+import org.leroyjenkins.paymenttestapp.service.PaymentProcessingService;
+import org.leroyjenkins.paymenttestapp.service.validation.impl.PaymentProcessorValidatorImpl;
+import org.leroyjenkins.paymenttestapp.unit.service.AbstractUnitTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.when;
+
+class PaymentProcessorValidatorImplTest extends AbstractUnitTest {
+    @Mock
+    private PaymentProcessingService paymentProcessingService;
+
+    @InjectMocks
+    private PaymentProcessorValidatorImpl paymentProcessorValidator;
+
+    @Test
+    void Should_ThrowException_When_validatePaymentProcessorIsCalledAndPaymentProcessorDoesntRegistered() {
+        String paymentProcessor = "payment-processor";
+        when(paymentProcessingService.isPaymentProcessorRegistered(paymentProcessor)).thenReturn(false);
+
+        assertThatThrownByIsTheSameAs(() -> paymentProcessorValidator.validatePaymentProcessor(paymentProcessor),
+                new PaymentProcessorNotRegisteredException(paymentProcessor));
+    }
+
+    @Test
+    void Should_DoNothing_When_validatePaymentProcessorIsCalledAndPaymentProcessorRegistered() {
+        String paymentProcessor = "payment-processor";
+        when(paymentProcessingService.isPaymentProcessorRegistered(paymentProcessor)).thenReturn(true);
+
+        assertThatCode(() -> paymentProcessorValidator.validatePaymentProcessor(paymentProcessor))
+                .doesNotThrowAnyException();
+    }
+}
